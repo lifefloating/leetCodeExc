63ad3ea51d9e3aca37d7f241be371a9a7d3420cb
curd_boy <imshuazi@126.com>
2019-09-16 20:41:39 +0800

    20

---
+++ 20.有效的括号.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=20 lang=python
+#
+# [20] 有效的括号
+#
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        dic = {
+            '(': ')',
+            '{': '}',
+            '[': ']',
+            '?': '?'
+        }
+        stack = ['?']
+        for c in s:
+            if c in dic: 
+                stack.append(c)
+            elif dic[stack.pop()] != c: 
+                return False 
+        return len(stack) == 1
+        

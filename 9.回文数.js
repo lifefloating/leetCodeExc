8a65de56ed8e29d4c5aa8fd1056cd61218b9f7cf
curd_boy <imshuazi@126.com>
2019-09-16 21:28:28 +0800

    回文优化

--- 9.回文数.js
+++ 9.回文数.js
@@ -7,25 +7,37 @@
  * @param {number} x
  * @return {boolean}
  */
+// 121 
 var isPalindrome = function(x) {
-    var ret = 0
-    var realx = x
+    // var ret = 0
+    // var realx = x
     if (x < 0) {
         return false
     }
     if (x < 10) {
         return true
     }
-    while (parseInt(x/10)) {
-        ret = 10 * ret + x - 10 * parseInt(x / 10);
-        x = parseInt(x/10)
+    var xStr = x.toString()
+    var tmp = ''
+    for (var i= xStr.length -1; i >= 0; i--) {
+        tmp += xStr[i]
     }
-    ret = 10 * ret + x
-    if (realx == ret) {
+    if(tmp == xStr) {
         return true
     } else {
         return false
     }
+
+    // while (parseInt(x/10)) {
+    //     ret = 10 * ret + x - 10 * parseInt(x / 10);
+    //     x = parseInt(x/10)
+    // }
+    // ret = 10 * ret + x
+    // if (realx == ret) {
+    //     return true
+    // } else {
+    //     return false
+    // }
 
 };
 
